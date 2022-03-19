7d37b8cd5ac7d729818ae44bd7e4b1484ccc9480
Emma Abel Bitsch <emma.abel.bitsch@gmail.com>
2022-03-19 23:31:54 +0100

    Adding JS - api and temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+}
+let apiKey = "6cd400507dc4152e1e63d463507ab0e3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);

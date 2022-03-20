ef58730ae685b77ac9e98207c2b9f183c8f77ffe
Emma Abel Bitsch <emma.abel.bitsch@gmail.com>
2022-03-20 11:53:47 +0100

    Relevant icon added from API

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ let timeElement = document.querySelector("#time");
 timeElement.innerHTML = `${hour}:${minutes}`;
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
@@ -22,6 +21,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let feelsLikeElement = document.querySelector("#feelsLike");
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "6cd400507dc4152e1e63d463507ab0e3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=${apiKey}&units=metric`;
